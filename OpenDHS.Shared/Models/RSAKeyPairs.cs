86d8f2f5b49c9e08bf00e26e5916c3eb70f1c25b
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2023-11-30 12:15:26 +0100

    Add RSACryptoService

---
+++ OpenDHS.Shared/Models/RSAKeyPairs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace OpenDHS.Shared.Models
+{
+    public class RSAKeyPairs
+    {
+        public RSAParameters  PublicKey { get; set; }
+        public RSAParameters PrivateKey { get; set; }
+
+     
+    }
+
+    public static class RSAKeyPairsExtensions {
+        public static string GetStringPublicKey(this RSAKeyPairs rsaKeyPairs)
+        {
+            var sw = new StringWriter();
+            var xmlSerializer = new XmlSerializer(typeof(RSAParameters));
+            xmlSerializer.Serialize(sw, rsaKeyPairs.PublicKey);
+            return sw.ToString();
+        }
+        public static string GetStringPrivateKey(this RSAKeyPairs rsaKeyPairs)
+        {
+            var sw = new StringWriter();
+            var xmlSerializer = new XmlSerializer(typeof(RSAParameters));
+            xmlSerializer.Serialize(sw, rsaKeyPairs.PrivateKey);
+            return sw.ToString();
+        }
+    }
+}
---
+++ OpenDHS.Shared/Services/RSACryptoService.cs
@@ -0,0 +1,51 @@
+﻿using OpenDHS.Shared.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace OpenDHS.Shared.Services
+{
+
+    public class RSACryptoService
+    {
+
+        public RSACryptoService(string publicXmlKey, string privateXmlKey)
+        {
+            var rsaCryptoServiceProvider = new RSACryptoServiceProvider(2048);
+            rsaCryptoServiceProvider.FromXmlString(publicXmlKey);
+        }
+
+        public static RSAKeyPairs GetKeyPairs() {
+
+            var rsaKeyPairs = new RSAKeyPairs();
+            var rsaCryptoServiceProvider = new RSACryptoServiceProvider(2048);
+            rsaKeyPairs.PublicKey = rsaCryptoServiceProvider.ExportParameters(false);
+            rsaKeyPairs.PrivateKey = rsaCryptoServiceProvider.ExportParameters(true);
+            return rsaKeyPairs;
+         }
+
+      
+        public static string Encrypt(string textToEncrypt, RSAParameters publicKey) {
+            var bytesToEncrypt = Encoding.UTF8.GetBytes(textToEncrypt);
+            
+            var rsaCryptoServiceProvider = new RSACryptoServiceProvider(2048);
+            rsaCryptoServiceProvider.ImportParameters(publicKey);
+
+            var encryptedData = rsaCryptoServiceProvider.Encrypt(bytesToEncrypt, true);
+            var base64Encrypted = Convert.ToBase64String(encryptedData);
+            return base64Encrypted;
+        }
+
+        public static string Decrypt(string textToDecrypt, RSAParameters privateKey)
+        {
+            var base64Encrypted = Convert.FromBase64String(textToDecrypt);
+
+            var rsaCryptoServiceProvider = new RSACryptoServiceProvider(2048);
+            rsaCryptoServiceProvider.ImportParameters(privateKey);
+
+            var decryptedData = rsaCryptoServiceProvider.Decrypt(base64Encrypted, true);
+
+            return  Encoding.UTF8.GetString(decryptedData);
+        }
+
+    }
+}
