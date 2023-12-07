75ca9d8f4e38485e0b29fd9b7095a005d4cb1480
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2023-12-07 08:22:30 +0100

    Add Finance Module Project

---
+++ OpenDHS.Finance/Areas/Finance/Pages/index.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OpenDHS.Finance.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
