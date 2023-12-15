947e0d4e6b932998789b9f37343396feceaf699b
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2023-12-15 15:42:28 +0100

    Add Offer Html as Razor Page

---
+++ Sites/OpenDHS.StudioMaggio.Site/Pages/Offer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OpenDHS.StudioMaggio.Site.Pages
+{
+    public class OfferModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
