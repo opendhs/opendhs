441f2b7923ecd7c43d436fab6931f9788e3c0f53
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2023-12-27 19:27:41 +0100

    Update User Creation Page

--- OpenDHS.Web/Areas/Manage/Pages/User/Create.cshtml.cs
+++ OpenDHS.Web/Areas/Manage/Pages/User/Create.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.DataProtection.Internal;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -13,30 +15,48 @@ namespace OpenDHS.Web.Areas.Manage.Pages.User
     public class CreateModel : PageModel
     {
         private readonly OpenDHS.Web.Data.OpenDHSDataContext _context;
-
-        public CreateModel(OpenDHS.Web.Data.OpenDHSDataContext context)
+        private readonly UserManager<UserEntity> _userManager;
+       
+        public CreateModel(OpenDHS.Web.Data.OpenDHSDataContext context, UserManager<UserEntity> userManager)
         {
+            _userManager = userManager;
             _context = context;
         }
 
         public IActionResult OnGet()
         {
+
             return Page();
         }
 
         [BindProperty]
-        public UserEntity UserEntity { get; set; } = default!;
+        public UserPageModel Model { get; set; } = default!;
         
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.Users == null || UserEntity == null)
+          if (!ModelState.IsValid || _context.Users == null || Model == null)
             {
                 return Page();
             }
 
-            _context.Users.Add(UserEntity);
+            // Teniendo las anotaciones compare esto no se debe verificar nunca! 
+            if (Model.Password != Model.PasswordConfirm) {
+                return Page();
+            }
+
+            //TODO: Terminar la logica y control de errores
+
+            var _user = Activator.CreateInstance<UserEntity>();
+            _user.Email = Model.Email;
+            _user.Name = Model.Name;
+            _user.Lastname = Model.Lastname;
+
+            var result = await _userManager.CreateAsync(_user, Model.Password);
+
+
+            _context.Users.Add(_user);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
---
+++ OpenDHS.Web/Areas/Manage/Pages/User/UserPageModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenDHS.Web.Areas.Manage.Pages.User
+{
+    public class UserPageModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contrasenna")]
+        public string Password { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string PasswordConfirm { get; set; } = string.Empty;
+
+    }
+}
