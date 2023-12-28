131b99a85d4bdcd1b05ac7f3a38cc1e13947cead
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2023-12-28 10:34:05 +0100

    Update Project Structure before repository migration

---
+++ OpenCRM.Manage/Areas/Manage/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OpenCRM.Manage.Areas.Manage.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
